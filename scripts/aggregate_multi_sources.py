dfb813eb3937b1c52637bab06ad4b749aebb088e
lanery <ryanilane@gmail.com>
2026-03-12 14:12:35 -0700

    Update aggregate_multi_sources.py
    - per Roman's request

--- scripts/aggregate_multi_sources.py
+++ scripts/aggregate_multi_sources.py
@@ -99,7 +99,7 @@ def main():
             if experiment == "96-well":
                 if "treatment" in df.columns:
                     t = df["treatment"].astype(str).str.lower()
-                    df["bead_present"] = t.apply(lambda x: True if "1 mm bead" in x else False)
+                    df["bead_present"] = t.apply(lambda x: True if "4.5 mm bead" in x else False)
                 elif "bead_present" in df.columns:
                     df["bead_present"] = (
                         df["bead_present"]
